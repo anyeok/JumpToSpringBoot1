b79f44e31d6287432cbad0ec8b62c076114adb05
anyeok <https://github.com/anyeok>
2024-09-03 11:38:15 +0900

    Hello World complete

---
+++ src/main/java/com/sld/sdd2/HelloController.java
@@ -0,0 +1,15 @@
+package com.sld.sdd2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello () {
+        return "Hello World";
+    }
+}

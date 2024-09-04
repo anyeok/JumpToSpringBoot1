f43cb1e5c7308ff5d2de768645b9c00ce2cf1458
anyeok <https://github.com/anyeok>
2024-09-04 12:29:27 +0900

    questionservice complete

---
+++ src/main/java/com/sld/sdd2/answer/AnswerRepository.java
@@ -0,0 +1,6 @@
+package com.sld.sdd2.answer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+}
--- src/test/java/com/sld/sdd2/Basic1ApplicationTests.java
+++ src/test/java/com/sld/sdd2/Basic1ApplicationTests.java
@@ -1,13 +1,38 @@
 package com.sld.sdd2;
 
+import com.sld.sdd2.answer.AnswerRepository;
+import com.sld.sdd2.question.Question;
+import com.sld.sdd2.question.QuestionRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
 
 @SpringBootTest
 class Basic1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private QuestionRepository questionRepository;
 
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Transactional
+    @Test
+    void testJpa() {
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q1);  // 첫번째 질문 저장
+
+        Question q2 = new Question();
+        q2.setSubject("스프링부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        q2.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q2);  // 두번째 질문 저장
+    }
 }

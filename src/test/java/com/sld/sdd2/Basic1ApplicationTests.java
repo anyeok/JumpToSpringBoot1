a4601c0b7d6a1f753b905f41fa3c88c1d37fb021
anyeok <https://github.com/anyeok>
2024-09-05 11:17:31 +0900

    question HTML complete

--- src/test/java/com/sld/sdd2/Basic1ApplicationTests.java
+++ src/test/java/com/sld/sdd2/Basic1ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.sld.sdd2;
 
+import com.sld.sdd2.answer.Answer;
 import com.sld.sdd2.answer.AnswerRepository;
 import com.sld.sdd2.question.Question;
 import com.sld.sdd2.question.QuestionRepository;
@@ -8,7 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
@@ -23,16 +28,14 @@ class Basic1ApplicationTests {
     @Transactional
     @Test
     void testJpa() {
-        Question q1 = new Question();
-        q1.setSubject("sbb가 무엇인가요?");
-        q1.setContent("sbb에 대해서 알고 싶습니다.");
-        q1.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q1);  // 첫번째 질문 저장
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
 
-        Question q2 = new Question();
-        q2.setSubject("스프링부트 모델 질문입니다.");
-        q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q2);  // 두번째 질문 저장
+        List<Answer> answerList = q.getAnswerList();
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
     }
 }
+
